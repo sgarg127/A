3edd499cf52c512607fc9316d82976cd2b8f806d
Manas Kundu <manas.kundu@460-ENGG-MANAS.local>
2023-11-28 23:02:50 +0530

    first commit

---
+++ FSbackend/secrets.py
@@ -0,0 +1,2 @@
+DJANGO_SECRET_KEY = 'django-insecure-jzcnl#g^u_yz^n9vgevyn19w+tg$cl&pxj62hd#4bg9-$jnow6'
+JWT_SECRET_KEY = "your jwt signing secret key"
---
+++ backend_auth/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BackendAuthConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'backend_auth'
